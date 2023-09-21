f2208598ada5e5592f02a38f76cd7f12ac358452
Yehor Azyrankulov <egorkavin221100@gmail.com>
2023-09-22 01:11:17 +0300

    build: run prettier

--- backend_sanity/sanity.cli.ts
+++ backend_sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'a7026azy',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
