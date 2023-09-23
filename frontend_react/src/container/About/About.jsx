7fdae7929f17c37ad288020edbbb61b3b0f94ba3
Yehor Azyrankulov <egorkavin221100@gmail.com>
2023-09-23 05:39:26 +0300

    feat: add <About />

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { getSpheres, urlFor } from '../../sanity';
 
 import './About.styles.scss';
 
 const About = () => {
-  return <div>About</div>;
+  const [spheres, setSpheres] = useState([]);
+
+  useEffect(() => {
+    getSpheres().then((spheres) => setSpheres(spheres));
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That <span>Good Development</span>
+        <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {spheres.map((sphere) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={sphere.title}
+          >
+            <img src={urlFor(sphere.imgUrl)} alt={sphere.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {sphere.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {sphere.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
