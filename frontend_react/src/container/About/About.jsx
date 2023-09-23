4d2faff6b07075d0c34992b77a51981428682d8a
Yehor Azyrankulov <egorkavin221100@gmail.com>
2023-09-23 06:09:12 +0300

    feat: add <AppWrap /> HOC

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { getSpheres, urlFor } from '../../sanity';
+import { AppWrap } from '../../wrapper';
 
 import './About.styles.scss';
 
@@ -43,4 +44,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
