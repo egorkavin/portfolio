34f32a3f7e274e6a7ab5924a45b98352000acc66
Yehor Azyrankulov <egorkavin221100@gmail.com>
2023-09-23 05:38:59 +0300

    feat: add sanity client

---
+++ frontend_react/src/sanity.js
@@ -0,0 +1,19 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2023-09-23',
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+export async function getSpheres() {
+  const spheres = await client.fetch('*[_type == "spheres"]');
+  return spheres;
+}
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
